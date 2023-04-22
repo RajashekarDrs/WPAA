a7c6c00f12e091edc53df0477b5f380f1db34a28
RajashekarDrs <rajashekar0581@gmail.com>
2023-04-22 13:33:15 +0530

    demo is commited

---
+++ src/test/java/GitTest/TestIttt.java
@@ -0,0 +1,19 @@
+package GitTest;
+
+import org.testng.annotations.Test;
+
+public class TestIttt {
+	@Test
+	public void sample()
+	{
+		
+		
+		System.out.println("sample     demo");
+		System.out.println("Murthi is a baaaaad boy but he is    a inocent boy");
+		System.out.println("Conflict practicre");
+		
+		
+		
+	}
+
+}
